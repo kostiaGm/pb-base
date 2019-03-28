004e8674a5281c6acc4cc84d98d0953fd5c9ab28
kostya <kostiaGm@gmail.com>
2019-03-28 19:10:05 +0200

    f1

--- config/web.php
+++ config/web.php
@@ -11,6 +11,13 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+
+    'modules' => [
+        'recept' => [
+            'class' => 'pb\core\recept\Recept',
+        ],
+    ],
+    
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
